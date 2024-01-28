5a584db00e86e0406220400fde53105bfcd0fb6c
Eduardo Luz <luz.eduardo@gmail.com>
2024-01-28 09:00:48 +0000

    adding initial interface to interact with the API

---
+++ interacting/todo/cmd/todo/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"eduardo-luz.eu/cmdgo/interating/todo"
+)
+
+const todoFilename = ".todo.json"
+
+func main() {
+	l := &todo.List{}
+
+	if err := l.Get(todoFilename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFilename); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+
+}
