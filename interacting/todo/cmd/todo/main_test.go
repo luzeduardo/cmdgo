53540d9ff615b33f7b31a6ce720ec996f11d75bf
Eduardo Luz <luz.eduardo@gmail.com>
2024-01-29 22:50:06 +0000

    reading task from stdin or flag .Args

--- interacting/todo/cmd/todo/main_test.go
+++ interacting/todo/cmd/todo/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -57,6 +58,39 @@ func TestTodoCLI(t *testing.T) {
 			t.Fatal(err)
 		}
 		expected := fmt.Sprintf(" 1: %s\n", task)
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead \n", expected, string(out))
+		}
+	})
+
+	t.Run("AddNewTaskFromArguments", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add", task)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+	task2 := "test task number 1"
+	t.Run("AddNewTaskFromSTDIN", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := fmt.Sprintf(" 1: %s\n 2: %s\n", task, task2)
 		if expected != string(out) {
 			t.Errorf("Expected %q, got %q instead \n", expected, string(out))
 		}
